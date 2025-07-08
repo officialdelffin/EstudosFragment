3f92c515e7de3832dde1fa115d7000a90800b44f
officialdelffin <workdelffin@gamil.com>
2025-07-07 21:59:03 -0300

    Commit estudos de fragments 1

---
+++ app/src/main/java/com/example/fragment/fragments/ConversasFragment.kt
@@ -0,0 +1,33 @@
+package com.example.fragment.fragments
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragment.R
+
+// Criando uma classe que herda de fragment :
+class ConversasFragment : Fragment() {
+
+    // Criando uma função que infla o layout do fragment :
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflando o fragmento e definindo o layout :
+        val view = inflater.inflate(
+
+            R.layout.fragment_conversas,
+            container,
+            false
+
+        )
+
+        // Retornando a variavel de inflação :
+        return view
+
+    }
+
+}
