26e9d21b3d6514c44a5ff26c3f04035777c8a2a5
officialdelffin <workdelffin>
2025-07-08 09:58:29 -0300

    Commit estudos de fragments 3

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fragment
 
+// Imports :
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/example/fragment/fragments/StatusFragment.kt
+++ app/src/main/java/com/example/fragment/fragments/StatusFragment.kt
@@ -1,5 +1,6 @@
 package com.example.fragment.fragments
 
+// Imports :
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
